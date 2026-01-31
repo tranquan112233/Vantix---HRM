f4ccd47d3b36bf891b5c46bb2ee221ace9a4a902
Hà Gia Bảo <baohg82@gmail.com>
2026-01-31 21:03:40 +0700

    Thêm giao diện chạy demo CheckIn

--- Vantix_Web/src/App.vue
+++ Vantix_Web/src/App.vue
@@ -1,9 +1,11 @@
   <template>
-    <Auth />
-    <AppHeader />
+<!--    <Auth />-->
+<!--    <AppHeader />-->
+    <AttendanceVue />
   </template>
 
   <script setup>
   import Auth from './components/views/Auth.vue'
   import AppHeader from "./components/AppHeader.vue";
+  import AttendanceVue from "./components/AttendanceVue.vue";
   </script>
--- Vantix_Web/src/main.js
+++ Vantix_Web/src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import '@/assets/css/main.css'
+// import '@/assets/css/main.css'
 
 createApp(App).mount('#app')
