d9508289c1a40ba6bf11fe56f7a2fc46244f2d6b
TranHoangQuan <tranquan567098@gmail.com>
2026-02-06 13:41:52 +0700

    nothing

--- Vantix_HRM/src/main/java/poly/edu/vantix_hrm/entity/Role.java
+++ Vantix_HRM/src/main/java/poly/edu/vantix_hrm/entity/Role.java
@@ -5,13 +5,20 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Entity
+@Table(name = "roles")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
 public class Role {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Integer id;
 
+    @Column(name = "role_name", nullable = false, unique = true, length = 50)
+    @NotBlank(message = "Role name is required")
     private String roleName;
 
+    @Column(length = 255)
     private String description;
 }
-
